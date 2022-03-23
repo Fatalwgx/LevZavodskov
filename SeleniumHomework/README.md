2ed4348ed60b398423847d8b7470daa616d38613
Fatalwgx <98048609+Fatalwgx@users.noreply.github.com>
2022-03-23 10:51:39 +0300

    Delete SeleniumHomework directory

--- SeleniumHomework/README.md
+++
@@ -1,7 +0,0 @@
-### Selenium Webdriver + Python Autotests examples
-
----
-
-This directory contains autotests performed by me during the course
-
-Certification and additional info will be added once the course is completed
--- SeleniumHomework/pytestlessons/test_items.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-
-def test_cart_button_is_present(browser):
-    browser.get(link)
-    assert EC.presence_of_element_located((By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')), 'No such element'
